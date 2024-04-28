03bdcc8c480e2af5314297016247d54f0754d8a1
Luís Soares <lsoares@newstore.com>
2024-04-28 14:04:38 +0100

    cleanup

--- library.c
+++ library.c
@@ -2,23 +2,22 @@
 #include <stdlib.h>
 #include "library.h"
 
-Acervo *criarAcervo() {
-    return (Acervo *) malloc(sizeof(Acervo));
-}
-
-void adicionarLivro(Acervo *acervo, Livro livroNovo) {
+void adicionarLivro(Acervo *acervo, Livro livroNovo)
+{
     strcpy(acervo->livros[acervo->quantidade].titulo, livroNovo.titulo);
     strcpy(acervo->livros[acervo->quantidade].autor, livroNovo.autor);
     strcpy(acervo->livros[acervo->quantidade].genero, livroNovo.genero);
     acervo->quantidade++; // Incrementar o número de livros no Acervo
 }
 
-void removerLivro(Acervo *acervo, int indiceLivroARemover) {
+void removerLivro(Acervo *acervo, int indiceLivroARemover)
+{
     acervo->livros[indiceLivroARemover] = acervo->livros[acervo->quantidade - 1];
     acervo->quantidade--; // Atualizar o número de livros no Acervo
 }
 
-void atualizarLivro(Acervo *acervo, int indice, Livro livro) {
+void atualizarLivro(Acervo *acervo, int indice, Livro livro)
+{
     Livro *livroAEditar = &acervo->livros[indice];
     strcpy(livroAEditar->titulo, livro.titulo);
     strcpy(livroAEditar->autor, livro.autor);
