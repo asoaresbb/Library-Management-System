b55a5999aa808d72e40bb9918a90f0eba4ed87c9
Luís Soares <lsoares@newstore.com>
2024-04-28 22:59:19 +0100

    bem-vindo

--- main.c
+++ main.c
@@ -24,9 +24,9 @@ int main()
     adicionarLivro(&acervo, "Linguagem C", "Luís Damas", "literatura técnica");
     adicionarLivro(&acervo, "jQuery", "Luís Soares", "literatura técnica");
 
-    printf("\n🍽️ 🍽️ 🍽️ 🍽️ 🍽️ 🍽️ 🍽️ 🍽️ 🍽️ 🍽️ 🍽️ 🍽️\n");
-    printf("🍽️ 🍽️ 🍽️  Gestor de Bibilioteca  🍽️ 🍽️ 🍽️ \n");
-    printf("🍽️ 🍽️ 🍽️ 🍽️ 🍽️ 🍽️ 🍽️ 🍽️ 🍽️ 🍽️ 🍽️ 🍽️ \n\n");
+    printf("\n📖 📖 📖 📖 📖 📖 📖 📖 📖 📖 📖 📖\n");
+    printf("📖 📖 📖  Gestor de Bibilioteca  📖 📖 📖 \n");
+    printf("📖 📖 📖 📖 📖 📖 📖 📖 📖 📖 📖 📖 \n\n");
 
     while (1)
     {
