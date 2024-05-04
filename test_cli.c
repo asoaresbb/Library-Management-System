0c7f37cf290649c26c2134af420c4daa34590644
Luís Soares <lsoares@newstore.com>
2024-05-04 14:02:50 +0100

    usar regex

--- test_cli.c
+++ test_cli.c
@@ -3,9 +3,11 @@
 #include <string.h>
 #include <stdarg.h>
 #include <assert.h>
+#include <regex.h>
 
 void executarCli(char *resultadoEsperado, ...);
-void resultadoContem(const char *resultado, const char *textoEsperado);
+void resultadoContemTexto(const char *resultado, const char *texto);
+void resultadoContemRegex(const char *resultado, const char *regex);
 void test_adicionar_livro();
 void test_remover_livro();
 void test_emprestar_livro();
@@ -19,18 +21,6 @@ int main()
     return 0;
 }
 
-void resultadoContem(const char *resultado, const char *textoEsperado)
-{
-    if (!strstr(resultado, textoEsperado))
-    {
-        printf("------------------------------------------\n");
-        printf("%s\n", resultado);
-        printf("------------------------------------------\n");
-        printf("❌ O resultado acima não contém o texto:\n%s\n", textoEsperado);
-        exit(1);
-    }
-}
-
 void test_adicionar_livro()
 {
     char resultadoEsperado[5000];
@@ -40,8 +30,8 @@ void test_adicionar_livro()
                 "l", "s",                                 // listar livros e sair
                 NULL);
 
-    resultadoContem(resultadoEsperado, ": Java Basics (Helbert)");
-    resultadoContem(resultadoEsperado, "A sair do programa...");
+    resultadoContemRegex(resultadoEsperado, "[0-9]+: Java Basics \\(Helbert\\)");
+    resultadoContemTexto(resultadoEsperado, "A sair do programa...");
 }
 
 void test_remover_livro()
@@ -53,8 +43,8 @@ void test_remover_livro()
                 "l", "s",  // listar livros e sair
                 NULL);
 
-    resultadoContem(resultadoEsperado, "Livro removido");
-    resultadoContem(resultadoEsperado, "A sair do programa...");
+    resultadoContemTexto(resultadoEsperado, "Livro removido");
+    resultadoContemTexto(resultadoEsperado, "A sair do programa...");
 }
 
 void test_emprestar_livro()
@@ -66,8 +56,8 @@ void test_emprestar_livro()
                 "i", "s",        // listar empréstimos e sair
                 NULL);
 
-    resultadoContem(resultadoEsperado, "3      - Linguagem C               | 103");
-    resultadoContem(resultadoEsperado, "A sair do programa...");
+    resultadoContemTexto(resultadoEsperado, "3      - Linguagem C               | 103");
+    resultadoContemTexto(resultadoEsperado, "A sair do programa...");
 }
 
 void executarCli(char *resultadoEsperado, ...)
@@ -77,19 +67,44 @@ void executarCli(char *resultadoEsperado, ...)
     char comandoComExecutavel[1035];
     char comando[5000] = "";
     char *arg;
+    // envia comandos
     while ((arg = va_arg(textosUtilizador, char *)) != NULL)
-    {
-        strcat(comando, arg);
-        strcat(comando, "\n");
-    }
-
+        sprintf(comando + strlen(comando), "%s\n", arg);
+    va_end(textosUtilizador);
     sprintf(comandoComExecutavel, "echo '%s' | ./cli.exe", comando);
+    // coleta output do programa com um pipe de leitura
     FILE *pipe = popen(comandoComExecutavel, "r");
     assert(pipe);
-    resultadoEsperado[0] = '\0'; // Inicializa a string vazia
-    char path[1035];
+    resultadoEsperado[0] = '\0';
+    char path[1000];
     while (fgets(path, sizeof(path), pipe))
         strcat(resultadoEsperado, path);
     pclose(pipe);
-    va_end(textosUtilizador);
+}
+
+void resultadoContemTexto(const char *resultado, const char *texto)
+{
+    if (!strstr(resultado, texto))
+    {
+        printf("------------------------------------------\n");
+        printf("%s\n", resultado);
+        printf("------------------------------------------\n");
+        printf("❌ O resultado acima não contém o texto:\n%s\n", texto);
+        exit(1);
+    }
+}
+
+void resultadoContemRegex(const char *resultado, const char *regexPattern)
+{
+    regex_t regex;
+    assert(regcomp(&regex, regexPattern, REG_EXTENDED) != REG_NOMATCH);
+    if (regexec(&regex, resultado, 0, NULL, 0))
+    {
+        printf("------------------------------------------\n");
+        printf("%s\n", resultado);
+        printf("------------------------------------------\n");
+        printf("❌ O resultado acima não contém o padrão:\n%s\n", regexPattern);
+        exit(1);
+    }
+    regfree(&regex);
 }
